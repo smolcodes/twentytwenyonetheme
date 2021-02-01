c92c5875301c1696c4f9b771e6c7dce4321f6939
Smol Codes <rbrittbusler92@gmail.com>
2021-01-31 21:36:55 -0500

    I think I fixed purgecss

--- tailwind.config.js
+++ tailwind.config.js
@@ -87,5 +87,5 @@ module.exports = {
     extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
-  purge: ["./src/**/*.js", "./src/**/*.html", "./src/**/*.svg"],
+  purge: ["./src/**/*.js", "./src/**/*.njk", "./src/**/*.svg"],
 };
